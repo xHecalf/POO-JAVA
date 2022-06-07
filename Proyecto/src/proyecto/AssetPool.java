aff5d1401dd3527cefe3cb948434e9daea0d9585
Hector Alfonso <99728147+xHecalf@users.noreply.github.com>
2022-06-06 23:46:42 -0700

    Add files via upload

---
+++ Proyecto/src/proyecto/AssetPool.java
@@ -0,0 +1,32 @@
+package proyecto;
+
+import proyecto.components.Sprite;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AssetPool {
+    static Map<String, Sprite> sprites = new HashMap<>();
+
+    public static boolean hasSprite(String pictureFile) {
+        return AssetPool.sprites.containsKey(pictureFile);
+    }
+
+    public static Sprite getSprite(String pictureFile) {
+        if (AssetPool.hasSprite(pictureFile)) {
+            return AssetPool.sprites.get(pictureFile);
+        }
+
+        return null;
+    }
+
+    public static void addSprite(String pictureFile, Sprite sprite) {
+        if (!AssetPool.hasSprite(pictureFile)) {
+            AssetPool.sprites.put(pictureFile, sprite);
+        }
+    }
+
+    public static void clearSprites() {
+        AssetPool.sprites.clear();
+    }
+}
